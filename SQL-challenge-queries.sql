761bc947c06d4efef275c03da103a38efba50699
Jeannaej <jeannaejy22@gmail.com>
2021-03-11 01:16:52 -0600

    Completed queries

--- SQL-challenge-queries.sql
+++ SQL-challenge-queries.sql
@@ -110,7 +110,43 @@ JOIN  employees ON
 employees.emp_no = dept_emp.emp_no;
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT last_name,
+	first_name,
+	sex
+FROM employees
+WHERE (first_name = 'Hercules' AND last_name LIKE 'B%');
+
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT departments.dept_name,
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name
+FROM departments
+JOIN dept_emp ON
+departments.dept_no = dept_emp.dept_no
+JOIN  employees ON
+employees.emp_no = dept_emp.emp_no
+WHERE departments.dept_name = 'Sales';
+
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT departments.dept_name,
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name
+FROM departments
+JOIN dept_emp ON
+departments.dept_no = dept_emp.dept_no
+JOIN  employees ON
+employees.emp_no = dept_emp.emp_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name ='Development';
+
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS total_employees
+FROM employees
+GROUP BY last_name
+ORDER BY total_employees desc;
+
 -- "Search your ID number." Your employee ID number is 499942
+SELECT * 
+FROM employees
+WHERE emp_no = 499942;
